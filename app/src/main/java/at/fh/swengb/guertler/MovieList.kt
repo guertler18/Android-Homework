901a7a082d97bbcee00bdff0aa4f912b5a6948cf
guertler18 <floriangurtler@Florians-MacBook-Pro-3.local>
2019-12-11 12:28:59 +0100

    Version 1.0

---
+++ app/src/main/java/at/fh/swengb/guertler/MovieList.kt
@@ -0,0 +1,12 @@
+package at.fh.swengb.guertler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+    }
+}
